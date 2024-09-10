bc373e2da00764f5ee29a8158572cae52e034b58
LittleMoki <steamlittle659@gmail.com>
2024-09-10 22:09:19 +0500

    delete backend

--- backend/routes/index.js
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express'
-import AuthRoutes from './AuthRoutes.js'
-
-const router = Router()
-
-router.use('/auth', AuthRoutes)
-
-export default router
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,18 +1,34 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
-  },
-  plugins: [],
-};
+	content: [
+		'./src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/components/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/modules/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/ui/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/app/**/*.{js,ts,jsx,tsx,mdx}',
+	],
+	theme: {
+		extend: {
+			colors: {
+				accent: '#0095e0',
+				'accent-dark-1': '#0086c9',
+				'accent-dark-2': '#0077b3',
+				'accent-dark-3': '#00689c',
+				'blue-dark': '#002538',
+				text: '#212529',
+			},
+			fontSize: {
+				base: '16px',
+			},
+			lineHeight: {
+				base: '1.45',
+			},
+			backgroundImage: {
+				'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+				'gradient-conic':
+					'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+			},
+		},
+	},
+	plugins: [],
+}
